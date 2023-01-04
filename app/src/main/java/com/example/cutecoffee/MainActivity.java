378c3e31945e567ee07b708b8f3ace9a1ec9c4cb
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-01-04 22:28:50 +0800

    Revert "delete sth"
    This reverts commit a8a11679a5a655b483999fa5ae5f75677fd970a6.

--- app/src/main/java/com/example/cutecoffee/MainActivity.java
+++ app/src/main/java/com/example/cutecoffee/MainActivity.java
@@ -36,9 +36,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.deleteDatabase("db");
-
         initView();
     }
 
